2282f7e8769dabf5eb3b0e0c8d8e00912b14fb81
sala7-2003 <mosala71352003@outlook.com>
2024-05-10 20:00:40 +0300

    The board using tkinter

--- board.py
+++ board.py
@@ -1,55 +1,12 @@
-import turtle
+from tkinter import Tk, Frame
 
-sc = turtle.Screen()
-sc.tracer(0)
-t = turtle.Turtle()
+window = Tk()
+window.geometry('600x600+300+30')
+window.title('Othello Game')
 
-def draw_square():
-    t.begin_fill()
-    for _ in range(4):
-        t.forward(50)
-        t.right(90)
-    t.end_fill()
+for i in range(8):
+    for j in range(8):
+        fr = Frame(window, width='75', height='75', bg='green', highlightbackground='black', highlightthickness=2)
+        fr.grid(row=i, column=j)
 
-def draw_horizontal_lines():
-    t.penup()
-    start_x = -200  # Starting x-coordinate
-    start_y = 200   # Starting y-coordinate
-    t.color('black')
-    t.pensize(2)
-    for i in range(9):
-        t.goto(start_x, start_y - i * 50)
-        t.pendown()
-        t.goto(start_x + 400, start_y - i * 50)
-        t.penup()
-
-def draw_vertical_lines():
-    t.penup()
-    start_x = -200  # Starting x-coordinate
-    start_y = 200   # Starting y-coordinate
-    t.color('black')
-    t.pensize(2)
-    for j in range(9):
-        t.goto(start_x + j * 50, start_y)
-        t.pendown()
-        t.goto(start_x + j * 50, start_y - 400)
-        t.penup()
-
-def draw_board():
-    t.penup()
-    start_x = -200  # Starting x-coordinate
-    start_y = 200   # Starting y-coordinate
-    for i in range(8):
-        for j in range(8):
-            t.goto(start_x + j * 50, start_y - i * 50)
-            t.pendown()
-            t.color('green')     # Set border color to black
-            draw_square()
-            t.penup()
-
-    draw_vertical_lines()  # Draw vertical lines
-    draw_horizontal_lines()  # Draw horizontal lines
-
-    turtle.done()  # Move outside of the loop
-
-draw_board()  # Call the draw_board function here
+window.mainloop()
