7f22555fb2cfc428073f9185430e719fbb6db2ce
sala7-2003 <mosala71352003@outlook.com>
2024-05-10 20:40:40 +0300

    1st update

--- board.py
+++ board.py
@@ -1,12 +1,21 @@
-from tkinter import Tk, Frame
+from tkinter import Tk, Frame, Label
 
 window = Tk()
-window.geometry('600x600+300+30')
+window.geometry('800x700')
+window.maxsize(900,900)# Adjust the window size as needed
 window.title('Othello Game')
 
+# Create the "You" and "Computer" labels
+you_label = Label(window, text='You', font=('Arial', 14))
+you_label.grid(row=0, column=0, padx=30, pady=20)
+
+computer_label = Label(window, text='Computer', font=('Arial', 14))
+computer_label.grid(row=1, column=0, padx=20, pady=20)
+
+# Create the 8x8 board
 for i in range(8):
     for j in range(8):
-        fr = Frame(window, width='75', height='75', bg='green', highlightbackground='black', highlightthickness=2)
-        fr.grid(row=i, column=j)
+        fr = Frame(window, width='60', height='60', bg='green', highlightbackground='black', highlightthickness=2)
+        fr.grid(row=i+2, column=4+j, padx=0, pady=0)
 
 window.mainloop()
